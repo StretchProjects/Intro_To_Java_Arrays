3e715339b05ee1dc40befdd693df77d820661772
Stretch Projects <StretchProjects@gmail.com>
2014-10-10 20:08:43 +0100

    Files to accompany Youtube series

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Author   : Stretch Projects
+ * Series   : Introduction to Java
+ * Topic    : Arrays
+ * Lecture  : http://youtu.be/G1II34QBFik
+ * Tutorial : http://youtu.be/wp5R5JCRtAQ
+ * License  : None.  Do with it as you wish! :)
+ */
+
+public class Main {
+	public static void main(String[] args) {
+		String[][] students = {{"john", "A"}, {"bob", "B"}};
+		
+		System.out.println(students[0][0]);
+	}
+}
